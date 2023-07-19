183cc32ca2b9c6de82a82ab81a5eeed592aeaddb
AntonKobyshev <antonkobyshev@gmail.com>
2023-07-20 00:09:35 +0300

    updt

--- render.js
+++ render.js
@@ -1,24 +1,24 @@
-const parentContainer = document.querySelector("body");
+// const parentContainer = document.querySelector("body");
 
-if (parentContainer) {
-  for (let i = 0; i < 7; i++) {
-    const div = document.createElement("div");
-    div.classList.add("column");
+// if (parentContainer) {
+//   for (let i = 0; i < 7; i++) {
+//     const div = document.createElement("div");
+//     div.classList.add("column");
 
-    const h2 = document.createElement("h2");
-    h2.setAttribute("data-type", "copy");
-    h2.textContent = "Text";
+//     const h2 = document.createElement("h2");
+//     h2.setAttribute("data-type", "copy");
+//     h2.textContent = "Text";
 
-    const button = document.createElement("button");
-    const icon = document.createElement("i");
-    icon.classList.add("fa-solid", "fa-lock-open");
-    icon.setAttribute("data-type", "lock");
-    button.appendChild(icon);
-    button.setAttribute("data-type", "lock");
+//     const button = document.createElement("button");
+//     const icon = document.createElement("i");
+//     icon.classList.add("fa-solid", "fa-lock-open");
+//     icon.setAttribute("data-type", "lock");
+//     button.appendChild(icon);
+//     button.setAttribute("data-type", "lock");
 
-    div.appendChild(h2);
-    div.appendChild(button);
+//     div.appendChild(h2);
+//     div.appendChild(button);
 
-    parentContainer.appendChild(div);
-  }
-}
+//     parentContainer.appendChild(div);
+//   }
+// }
--- script.js
+++ script.js
@@ -1,10 +1,40 @@
+const parentContainer = document.querySelector("body");
+
+if (parentContainer) {
+  for (let i = 0; i < 7; i++) {
+    const div = document.createElement("div");
+    div.classList.add("column");
+
+    const h2 = document.createElement("h2");
+    h2.setAttribute("data-type", "copy");
+    h2.textContent = "Text";
+
+    const button = document.createElement("button");
+    const icon = document.createElement("i");
+    icon.classList.add("fa-solid", "fa-lock-open");
+    icon.setAttribute("data-type", "lock");
+    button.appendChild(icon);
+    button.setAttribute("data-type", "lock");
+
+    div.appendChild(h2);
+    div.appendChild(button);
+
+    parentContainer.appendChild(div);
+  }
+}
+
 const columns = document.querySelectorAll(".column");
 
 document.addEventListener("keydown", (event) => {
-  event.preventDefault();
   if (event.code.toLowerCase() === "space") {
     setRandomColours();
   }
+});
+
+const generateColorBtn = document.querySelector(".generate-color");
+
+generateColorBtn.addEventListener("click", () => {
+  setRandomColours();
 });
 
 document.addEventListener("click", (event) => {
@@ -23,14 +53,6 @@ document.addEventListener("click", (event) => {
   }
 });
 
-// function generateRandomColor() {
-//     const hexCodes = "01234567890ABCD";
-//     let color = "";
-//     for (let i = 0; i < 6; i++) {
-//         color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
-//     }
-//     return "#" + color;
-// }
 
 function copyToClickboard(text) {
   return navigator.clipboard.writeText(text);
